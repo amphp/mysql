164e70d89431a0716ebc2c7eaca193d2ea57b383
Bob Weinand <bobwei9@hotmail.com>
2014-12-17 00:58:28 +0100

    Use utf8mb4 as primary encoding as suggested by @Danack

--- lib/ConnectionConfig.php
+++ lib/ConnectionConfig.php
@@ -14,8 +14,8 @@ class ConnectionConfig {
 	public $busy = null;
 	public $exceptions = true;
 	public $restore = null;
-	public $binCharset = 0x21; // utf8_general_ci
-	public $charset = "utf8";
-	public $collate = "utf8_general_ci";
+	public $binCharset = 45; // utf8mb4_general_ci
+	public $charset = "utf8mb4";
+	public $collate = "utf8mb4_general_ci";
 	public $key = [];
 }
