fd33e7909437d3fe82f1c39189daa95633240de1
Dmitry Balabka <dmitry.balabka@gmail.com>
2017-09-06 22:49:37 +0300

    Initial implementation of benchmarking

---
+++ benchmarks/AbstractBench.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Amp\Mysql\Bench;
+
+class AbstractBench {
+    /** @var string */
+    protected $host = '127.0.0.1';
+
+    /** @var string */
+    protected $user = 'root';
+
+    /** @var string */
+    protected $pass = '';
+}
---
+++ benchmarks/QueryBench.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Amp\Mysql\Bench;
+
+use Amp\Mysql\Connection;
+use Amp\Mysql\Internal\ConnectionConfig;
+use Amp\Mysql\Pool as ConnectionPool;
+use Amp\Mysql\ResultSet;
+use PhpBench\Benchmark\Metadata\Annotations\AfterMethods;
+use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
+use PhpBench\Benchmark\Metadata\Annotations\Iterations;
+use PhpBench\Benchmark\Metadata\Annotations\OutputTimeUnit;
+use PhpBench\Benchmark\Metadata\Annotations\Revs;
+use PhpBench\Benchmark\Metadata\Annotations\Warmup;
+use function Amp\call;
+use function Amp\Promise\wait;
+
+/**
+ * @BeforeMethods({"init"})
+ * @AfterMethods({"onAfterMethods"})
+ * @Iterations(1)
+ * @Revs(100)
+ * @Warmup(1)
+ * @OutputTimeUnit("milliseconds", precision=5)
+ */
+class QueryBench extends AbstractBench {
+    /** @var  ConnectionPool */
+    protected $connectionPool;
+
+    /** @var  Connection */
+    protected $connection;
+
+    /** @var  \PDO */
+    protected $pdoConnection;
+
+    /** @var int */
+    protected $maxQueries = 10;
+
+    /** @var int */
+    protected $poolLimit = 10;
+
+    public function init() {
+        $config = ConnectionConfig::parseConnectionString("host=$this->host;user=$this->user;pass=$this->pass");
+        $this->connectionPool = new ConnectionPool($config, $this->poolLimit);
+        $connectionPromise = Connection::connect($config);
+        $this->connection = wait($connectionPromise);
+        $this->pdoConnection = new \PDO("mysql:host=$this->host;port=3306", $this->user, $this->pass);
+    }
+
+    public function onAfterMethods() {
+        $this->connectionPool->close();
+        $this->connection->close();
+    }
+
+    public function benchPdoQueries() {
+        foreach (range(1, $this->maxQueries) as $ii) {
+            $resultSet = $this->pdoConnection->query("SELECT $ii");
+            $resultSet->fetch(\PDO::FETCH_ASSOC);
+        }
+    }
+
+    public function benchSyncQueries() {
+        wait(call(function () {
+            $connection = $this->connection;
+            foreach (range(1, $this->maxQueries) as $i) {
+                /** @var ResultSet $resultSet */
+                $resultSet = yield $connection->query("SELECT $i");
+                yield $resultSet->advance();
+            }
+        }));
+    }
+
+    public function benchAsyncQueries() {
+        wait(call(function () {
+            $connection = $this->connection;
+            /** @var ResultSet[] $resultSets */
+            $resultSets = yield array_map(function ($i) use ($connection) {
+                return $connection->query("SELECT $i");
+            }, range(1, $this->maxQueries));
+            yield array_map(function ($resultSet) {
+                /** @var ResultSet $resultSet */
+                return $resultSet->advance();
+            }, $resultSets);
+        }));
+    }
+
+    public function benchAsyncQueriesUsingPool() {
+        wait(call(function () {
+            $connection = $this->connectionPool;
+            /** @var ResultSet[] $resultSets */
+            $resultSets = yield array_map(function ($i) use ($connection) {
+                return $connection->query("SELECT $i");
+            }, range(1, $this->maxQueries));
+            yield array_map(function ($resultSet) {
+                /** @var ResultSet $resultSet */
+                return $resultSet->advance();
+            }, $resultSets);
+        }));
+    }
+}
