5f974878318757f976e647f9f2e6c90dff7b57ad
Aaron Piotrowski <aaron@trowski.com>
2019-09-26 23:34:10 -0500

    Update tests

--- test/ConnectionTest.php
+++ test/ConnectionTest.php
@@ -2,56 +2,40 @@
 
 namespace Amp\Mysql\Test;
 
-use Amp\Loop;
+use Amp\Mysql\CancellableConnector;
 use Amp\Mysql\Connection;
 use Amp\Mysql\ConnectionConfig;
-use Amp\Mysql\TimeoutConnector;
 use Amp\Promise;
-use function Amp\Mysql\connect;
 
 class ConnectionTest extends LinkTest
 {
     protected function getLink(string $connectionString): Promise
     {
-        return (new TimeoutConnector)->connect(ConnectionConfig::fromString($connectionString));
+        return (new CancellableConnector())->connect(ConnectionConfig::fromString($connectionString));
     }
 
     public function testConnect()
     {
-        $complete = false;
-        Loop::run(function () use (&$complete) {
-            /** @var Connection $db */
-            $db = yield connect(ConnectionConfig::fromString("host=".DB_HOST." user=".DB_USER." pass=".DB_PASS." db=test"));
+        /** @var Connection $db */
+        $db = yield Connection::connect(ConnectionConfig::fromString("host=".DB_HOST." user=".DB_USER." pass=".DB_PASS." db=test"));
 
-            /* use an alternative charset... Default is utf8mb4_general_ci */
-            yield $db->setCharset("latin1_general_ci");
+        $this->assertInstanceOf(Connection::class, $db);
 
-            $db->close();
-            $complete = true;
-        });
-        $this->assertTrue($complete, "Database commands did not complete.");
-    }
+        /* use an alternative charset... Default is utf8mb4_general_ci */
+        yield $db->setCharset("latin1_general_ci");
 
-    /**
-     * @expectedException \Error
-     * @expectedExceptionMessage Host must be provided in connection string
-     */
-    public function testInvalidConnectionString()
-    {
-        $promise = connect(ConnectionConfig::fromString("username=".DB_USER));
+        $db->close();
     }
 
     public function testDoubleClose()
     {
-        Loop::run(function () {
-            /** @var Connection $db */
-            $db = yield $this->getLink("host=".DB_HOST.";user=".DB_USER.";pass=".DB_PASS.";db=test");
+        /** @var Connection $db */
+        $db = yield $this->getLink("host=".DB_HOST.";user=".DB_USER.";pass=".DB_PASS.";db=test");
 
-            $db->close();
+        $db->close();
 
-            $this->assertFalse($db->isAlive());
+        $this->assertFalse($db->isAlive());
 
-            $db->close(); // Should not throw an exception.
-        });
+        $db->close(); // Should not throw an exception.
     }
 }
