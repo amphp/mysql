935d0031c1ffa6f7a38e00ce9cb0b5491800f814
Aaron Piotrowski <aaron@trowski.com>
2020-10-29 12:08:13 -0500

    Update test bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 
+namespace Amp\Mysql\Test;
+
+use Amp\ByteStream;
+use Amp\Loop;
+use Amp\Process\Process;
+use function Amp\delay;
+
 require __DIR__.'/../vendor/autoload.php';
 
 const DB_HOST = 'localhost:10101';
@@ -7,69 +14,75 @@ const DB_USER = 'root';
 const DB_PASS = '';
 const DB_NAME = 'test';
 
-$pipes = [];
-$buf = "";
-
-/* cleanup in case it wasn't terminated properly... */
-$pidfile = __DIR__."/mysql.pid";
-if (\file_exists($pidfile)) {
-    if (\stripos(PHP_OS, "win") === 0) {
-        \shell_exec("Taskkill /PID ".\file_get_contents($pidfile)." /F");
-    } else {
-        \shell_exec("kill -9 `cat '$pidfile'` 2>/dev/null");
+(function (): void {
+    /* cleanup in case it wasn't terminated properly... */
+    $pidfile = __DIR__ . "/mysql.pid";
+    if (\file_exists($pidfile)) {
+        if (\stripos(PHP_OS, "win") === 0) {
+            \shell_exec("Taskkill /PID " . \file_get_contents($pidfile) . " /F");
+        } else {
+            \shell_exec("kill -9 `cat '$pidfile'` 2>/dev/null");
+        }
+        \sleep(1);
     }
-    \sleep(1);
-}
-if (\file_exists(__DIR__."/mysql_db")) {
-    $rm_all = function ($dir) use (&$rm_all) {
-        $files = \glob("$dir/*");
-        if (\is_array($files)) {
-            foreach ($files as $file) {
-                if (\is_dir($file)) {
-                    $rm_all($file);
-                    \rmdir($file);
-                } else {
-                    \unlink($file);
+
+    if (\file_exists(__DIR__ . "/mysql_db")) {
+        $rm_all = function ($dir) use (&$rm_all) {
+            $files = \glob("$dir/*");
+            if (\is_array($files)) {
+                foreach ($files as $file) {
+                    if (\is_dir($file)) {
+                        $rm_all($file);
+                        \rmdir($file);
+                    } else {
+                        \unlink($file);
+                    }
                 }
             }
-        }
-    };
-    $rm_all(__DIR__ . "/mysql_db");
-} else {
-    @\mkdir(__DIR__ . "/mysql_db");
-}
+        };
+        $rm_all(__DIR__ . "/mysql_db");
+    } else {
+        @\mkdir(__DIR__ . "/mysql_db");
+    }
+})();
 
-$proc = \proc_open("mysqld --defaults-file=my.cnf --initialize-insecure", [2 => ["pipe", "w"]], $pipes, __DIR__);
-$stderr = $pipes[2];
-do {
-    if (!($row = \fgets($stderr)) || \preg_match("# \[ERROR\] #", $row)) {
-        print "\nERROR: Aborting, couldn't start mysql successfully\n$buf$row";
+Loop::run(function () {
+    print "\rCreating mysql server... ";
+
+    $dir = __DIR__;
+
+    $process = new Process("mysqld --defaults-file={$dir}/my.cnf --initialize-insecure", __DIR__);
+
+    yield $process->start();
+
+    $stderr = yield ByteStream\buffer($process->getStderr());
+
+    if (\preg_match("# \[ERROR\] #", $stderr)) {
+        print "\nERROR: Aborting, couldn't start mysql successfully\n{$stderr}";
         exit(127);
     }
-    $buf .= $row;
-} while (!\preg_match("(root@localhost is created with an empty password)", $row));
-\sleep(3); // :-(
-\proc_terminate($proc, 9);
-@\proc_terminate($proc, 9);
 
-$proc = \proc_open("mysqld --defaults-file=my.cnf --user=root", [2 => ["pipe", "w"]], $pipes, __DIR__);
+    yield $process->join();
 
-\register_shutdown_function(function () use ($proc) {
-    \proc_terminate($proc, 9);
+    $process = new Process("mysqld --defaults-file={$dir}/my.cnf --user=root", __DIR__);
+
+    yield $process->start();
+
+    print "\rStarting mysqld...       ";
+
+    yield delay(1000); // Give mysqld time to start.
+
+    print "\rCreating test database...";
+
+    $db = new \mysqli(DB_HOST, DB_USER, DB_PASS);
+    $db->query("CREATE DATABASE test");
+    $db->query("CREATE TABLE test.main (a INT(11), b INT(11))");
+    $db->query("INSERT INTO test.main VALUES (1, 2), (2, 3), (3, 4), (4, 5), (5, 6)");
+    $db->close();
+
+    print "\r";
+
+    \register_shutdown_function(function () use ($process): void {
+        $process->signal(\SIGTERM);
+    });
 });
-
-$stderr = $pipes[2];
-do {
-    if (!($row = \fgets($stderr)) || \preg_match("# \[ERROR\] #", $row)) {
-        print "\nERROR: Aborting, couldn't start mysql successfully\n$buf$row";
-        exit(127);
-    }
-    $buf .= $row;
-} while (!\preg_match("(^Version: '[0-9.a-zA-Z]+')", $row));
-
-
-$db = new mysqli(DB_HOST, DB_USER, DB_PASS);
-$db->query("CREATE DATABASE test");
-$db->query("CREATE TABLE test.main (a INT(11), b INT(11))");
-$db->query("INSERT INTO test.main VALUES (1, 2), (2, 3), (3, 4), (4, 5), (5, 6)");
-$db->close();
