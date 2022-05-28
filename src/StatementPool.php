ea8b6af236adae39851a76492cf3e5defb693b37
Aaron Piotrowski <aaron@trowski.com>
2022-05-28 10:23:36 -0500

    Update StatementPool for changes in sql-common

--- src/StatementPool.php
+++ src/StatementPool.php
@@ -4,20 +4,29 @@ namespace Amp\Mysql;
 
 use Amp\Sql\Common\StatementPool as SqlStatementPool;
 use Amp\Sql\Result as SqlResult;
-use Amp\Sql\Statement as SqlStatement;
 
 final class StatementPool extends SqlStatementPool implements Statement
 {
     private array $params = [];
 
-    protected function prepare(SqlStatement $statement): Statement
+    /**
+     * @psalm-suppress LessSpecificReturnStatement, MoreSpecificReturnType
+     */
+    protected function pop(): Statement
     {
-        \assert($statement instanceof Statement);
+        $statement = parent::pop();
 
-        $statement->reset();
+        try {
+            \assert($statement instanceof Statement);
 
-        foreach ($this->params as $paramId => $data) {
-            $statement->bind($paramId, $data);
+            $statement->reset();
+
+            foreach ($this->params as $paramId => $data) {
+                $statement->bind($paramId, $data);
+            }
+        } catch (\Throwable $exception) {
+            $this->push($statement);
+            throw $exception;
         }
 
         return $statement;
