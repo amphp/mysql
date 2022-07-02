e6c61c32f245400c46995fc65f6f87bbc4d717f8
Aaron Piotrowski <aaron@trowski.com>
2022-07-01 23:17:39 -0500

    Fix borked enum

--- src/Internal/SessionStateType.php
+++ src/Internal/SessionStateType.php
@@ -9,10 +9,10 @@ namespace Amp\Mysql\Internal;
  */
 enum SessionStateType: int
 {
-    public const SystemVariables = 0x00;
-    public const Schema = 0x01;
-    public const StateChange = 0x02;
-    public const Gtids = 0x03;
-    public const TransactionCharacteristics = 0x04;
-    public const TransactionState = 0x05;
+    case SystemVariables = 0x00;
+    case Schema = 0x01;
+    case StateChange = 0x02;
+    case Gtids = 0x03;
+    case TransactionCharacteristics = 0x04;
+    case TransactionState = 0x05;
 }
